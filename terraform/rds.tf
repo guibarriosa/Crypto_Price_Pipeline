d1c585d380afcfbe502e2446b998e720883d7945
Gui <guibarriosa5@gmail.com>
2026-03-31 16:13:14 +0100

    Added SG and Subnets to RDS

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -6,10 +6,22 @@ resource "aws_db_instance" "crypto_db" {
   engine_version       = "8.0"
   instance_class       = "db.t3.micro"
   username             = "gui"
-  password             = "barriosa123"
+  password             = var.db_password
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
-  
 
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.crypto_subnets.name
+
+  publicly_accessible = true
+  
 }
 
+resource "aws_db_subnet_group" "crypto_subnets" {
+  name       = "crypto_db_subnets"
+  subnet_ids = module.vpc.public_subnets
+
+  tags = {
+    Name = "Crypto DB Subnet Group"
+  }
+}
