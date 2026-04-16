1f8a1aeef286c067ee65aef3651fcf39e366b4b8
Gui <guibarriosa5@gmail.com>
2026-04-16 10:48:33 +0100

    Added force_destroy

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,6 +1,8 @@
 resource "aws_s3_bucket" "crypto_bucket" {
   bucket = "crypto-pipeline-gui"
 
+  force_destroy = true 
+
   tags = {
     Name        = "Crypto Pipeline Bucket"
     Environment = "Dev"
