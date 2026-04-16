5e71f4da68b21e2ece48ed1374d3192c7cf2a836
Gui <guibarriosa5@gmail.com>
2026-04-16 10:50:35 +0100

    Removed testing function

--- src/load.py
+++ src/load.py
@@ -19,13 +19,3 @@ def save_to_s3(data, prefix):
         ContentType="application/json"
     )
 
-
-
-'''def save_local(data):
-    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    filename = f"crypto_{timestamp}.json"
-    
-    with open(filename, "w") as f:
-        json.dump(data, f)
-
-    print(f"Saved: {filename}")'''
