1f48d9f629ea5c1736369f080d396ea576d927a3
Gui <guibarriosa5@gmail.com>
2026-04-17 15:58:51 +0100

    Changed to remote backend S3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = "crypto-pipeline-tfstates"
+    key    = "terraform/terraform.tfstate"
+    region = "us-east-1"
+  }
+  
   required_version = ">= 1.5.0"
 
   required_providers {
