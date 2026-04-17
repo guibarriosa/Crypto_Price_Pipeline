de10dbdd11b9ab7b8259761cfe508f1869c077c2
Gui <guibarriosa5@gmail.com>
2026-04-17 12:26:10 +0100

    Removed timestamp, was redundant

--- src/transform.py
+++ src/transform.py
@@ -2,13 +2,11 @@ from datetime import datetime
 
 def transform(data):
     transformed_data = []
-    timestamp = datetime.now().strftime("%HH-%MM-%SS")
 
     for coin, values in data.items():
         transformed_data.append({
             "coin": coin,
             "price_eur": values["eur"],
-            "timestamp": timestamp
         })
 
     return transformed_data
