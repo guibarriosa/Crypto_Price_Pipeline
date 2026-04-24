fd9a007ecfb72c8edf34c416eefadbea95262665
Gui <guibarriosa5@gmail.com>
2026-04-24 10:19:34 +0100

    Added security group for lambda

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -1,4 +1,6 @@
 resource "aws_security_group" "rds_sg" {
+  name        = "crypto-pipeline-rds-sg"
+  description = "Security group for RDS instance"
   vpc_id = module.vpc.vpc_id
 
   ingress {
@@ -17,6 +19,30 @@ resource "aws_security_group" "rds_sg" {
   }
 
   tags = {
-    Name = "RDS_SG"
+    Name = "Crypto Pipeline RDS SG"
+  }
+}
+
+resource "aws_security_group" "lambda_sg" {
+  name        = "crypto-pipeline-lambda-sg"
+  description = "Security group for Lambda function"
+  vpc_id      = module.vpc.vpc_id  
+
+  egress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.rds_sg.id]  
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Crypto Pipeline Lambda SG"
   }
 }
